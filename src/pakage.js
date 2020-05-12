b1c000c8b2b203bd02dec128f7eafeb7b0cfeab4
virginashmi15  <jvirginashmi>
2020-05-12 01:03:10 -0700

    this is my server  

---
+++ src/pakage.js
@@ -0,0 +1,23 @@
+{
+    "name": "readyassist-server",
+    "version": "1.0.0",
+    "description": "server",
+    "main": "index.js",
+    "scripts": {
+      "run": "node app/server.js" 
+    },
+    "repository": {
+      "type": "git",
+      "url": "git+https://github.com/virginashmi15/readyassist-server.git"
+    },
+    "author": "virgin",
+    "license": "ISC",
+    "bugs": {
+      "url": "https://github.com/virginashmi15/readyassist-server/issues"
+    },
+    "homepage": "https://github.com/virginashmi15/readyassist-server#readme",
+    "dependencies": {
+      "express": "^4.17.1"
+    }
+  }
+  
